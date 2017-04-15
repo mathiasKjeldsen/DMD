92afc2593180fe06aee2a18580089e5cf7022076
bjornSkalkam <bjorn@skalkam.dk>
2017-04-15 20:25:34 +0200

    added log-in.html and sign-up.html

---
+++ src/pages/log-in/log-in.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-log-in',
+  templateUrl: 'log-in.html'
+})
+export class LogInPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ src/pages/sign-up/sign-up.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-sign-up',
+  templateUrl: 'sign-up.html'
+})
+export class SignUpPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
