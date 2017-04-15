b4f3237df6e770cb90bb9b55f2d75524e1c2b560
bjornSkalkam <bjorn@skalkam.dk>
2017-04-15 20:34:31 +0200

    sign up button added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LogInPage } from '../log-in/log-in';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,10 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+swag() {
+    this.navCtrl.push(LogInPage);
+}
+
 
 }
