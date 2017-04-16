0feb2cb7a4797f7b07e47426e399983c53452710
mathiasKjeldsen <Mathias@mekbas.dk>
2017-04-16 21:09:14 +0200

    datepicker

--- src/pages/additional-info/additional-info.ts
+++ src/pages/additional-info/additional-info.ts
@@ -1,22 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HandifyPage } from '../handify/handify';
-
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 @Component({
   selector: 'page-additional-info',
   templateUrl: 'additional-info.html'
 })
 export class AdditionalInfoPage {
+  public additionalForm;
+  additionalInfo: { first: string, second: string, date: string} = { first: '', second: '', date: ''};
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
 
+    this.additionalForm = this.formBuilder.group({
+
+      'first': ['', Validators.compose([Validators.minLength(6), Validators.required])],
+      'second': ['', Validators.compose([Validators.minLength(6), Validators.required])],
+      'date': ['', Validators.compose([Validators.minLength(6), Validators.required])]
+
+    });
   }
-backToPreviousPage() {
-    this.navCtrl.pop();
-}
 
-goToHandify() {
+   isValid(field: string) {
+    let formField = this.additionalForm.get(field);
+    return formField.valid || formField.pristine;
+  }
+
+
+
+  backToPreviousPage() {
+    this.navCtrl.pop();
+  }
+
+  goToHandify() {
     this.navCtrl.setRoot(HandifyPage);
-}
+  }
 
 }
