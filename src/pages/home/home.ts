2da0447a6d96d2a6818fc029c73db374aee6cab5
mathiasKjeldsen <Mathias@mekbas.dk>
2017-04-16 18:37:38 +0200

    Validation home & buttons disabled

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,25 +2,38 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HandifyPage } from '../handify/handify';
 import { SignUpPage } from '../sign-up/sign-up';
+import { FormBuilder, Validators, FormGroup} from '@angular/forms';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public loginForm;
+  loginInfo: { email: string, password: string } = { email: '', password: '' };
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
+
+    this.loginForm = this.formBuilder.group({
+
+      'email': ['', Validators.compose([Validators.required, Validators.pattern(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)])],
+      'password': ['', Validators.compose([Validators.minLength(6), Validators.required])]
+    });
 
   }
 
-logIn() {
+  isValid(field: string) {
+    let formField = this.loginForm.get(field);
+    return formField.valid || formField.pristine;
+  }
+
+  logIn() {
     this.navCtrl.setRoot(HandifyPage);
-}
+  }
 
-goToSignUp() {
+  goToSignUp() {
     this.navCtrl.push(SignUpPage);
-}
-
-
+  }
 
 
 }
