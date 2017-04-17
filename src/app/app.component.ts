f5dcf58914109967472d9e9104c83e4bd2401b6f
mathiasKjeldsen <Mathias@mekbas.dk>
2017-04-17 05:08:14 +0200

    Tilføjet side-menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,50 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { HandifyPage } from '../pages/handify/handify';
 import { HomePage } from '../pages/home/home';
+
+
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  @ViewChild(Nav) nav: Nav;
+
+  pages: Array<{ title: string, component: any, icon: string }>;
+
+  constructor(public platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    this.pages = [
+      { title: 'Home', component: HomePage, icon: "md-home" },
+      { title: 'Settings', component: HandifyPage, icon: "ios-settings" },
+      { title: 'Calender overview', component: HandifyPage, icon: "ios-calendar-outline" },
+      { title: 'Exit Handify', component: HomePage, icon: "ios-exit-outline" }
+
+    ];
+
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
+
+  openPage(page) {
+    this.nav.setRoot(page.component);
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+    });
+  }
+
+  
+
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, MenuController } from 'ionic-angular';
 import { HandifyPage } from '../handify/handify';
 import { SignUpPage } from '../sign-up/sign-up';
-import { FormBuilder, Validators, FormGroup} from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'page-home',
@@ -12,7 +12,7 @@ export class HomePage {
   public loginForm;
   loginInfo: { email: string, password: string } = { email: '', password: '' };
 
-  constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder, public menuCtrl: MenuController) {
 
     this.loginForm = this.formBuilder.group({
 
@@ -35,5 +35,8 @@ export class HomePage {
     this.navCtrl.push(SignUpPage);
   }
 
+ionViewDidEnter() {
+    this.menuCtrl.swipeEnable(false, 'mainMenu');
+  }
 
 }
