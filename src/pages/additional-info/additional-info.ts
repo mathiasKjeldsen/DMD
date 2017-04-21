016179748af391b29262f021e51477709d063d03
bjornSkalkam <bjorn@skalkam.dk>
2017-04-21 12:49:32 +0200

    prøvet at lave additional info, kun 50% færdig

--- src/pages/additional-info/additional-info.ts
+++ src/pages/additional-info/additional-info.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController,
-  AlertController } from 'ionic-angular';
+import {
+  NavController, LoadingController,
+  AlertController
+} from 'ionic-angular';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ProfileData } from '../../providers/profile-data';
 import { HandifyPage } from '../handify/handify';
@@ -12,7 +14,7 @@ export class AdditionalInfoPage {
   public profileInfoForm;
   public dateForm;
   loading: any;
-  profileInfoInfo: { birthDate: string, address: string, zip: string, city: string, country: string } = { birthDate: '', address: '', zip: '', city: '', country: '',};
+  profileInfoInfo: { birthDate: string, address: string, zip: string, city: string, country: string } = { birthDate: '', address: '', zip: '', city: '', country: '', };
   dateInfo: { datepicker: string } = { datepicker: '' };
 
   constructor(public navCtrl: NavController, public formBuilder: FormBuilder, public profileData: ProfileData, public alertCtrl: AlertController, public loadingCtrl: LoadingController) {
@@ -46,41 +48,14 @@ export class AdditionalInfoPage {
     this.navCtrl.pop();
   }
 
-//  updateProfile() {
-//    this.profileData.updateDOB(this.profileInfoForm.data.birthDate);
-//    this.profileData.updateAddress(this.profileInfoForm.data.address);
-//    this.profileData.updateZip(this.profileInfoForm.data.zip);
-//    this.profileData.updateCity(this.profileInfoForm.data.city);
-//    this.profileData.updateCountry(this.profileInfoForm.data.country);
-//  }
-
-   updateProfile() {
-    if (!this.profileInfoForm.valid) {
-      console.log(this.profileInfoForm.value);
-    } else {
-      console.log("is this happening?");
-    this.profileData.updateCountry(this.profileInfoForm.data.country)
-    
-        .then(() => {
-          this.loading.dismiss().then(() => {
-            this.navCtrl.setRoot(HandifyPage); 
-          });
-        }, (error) => {
-          this.loading.dismiss().then(() => {
-            let alert = this.alertCtrl.create({
-              message: error.message,
-              buttons: [
-                {
-                  text: "Ok",
-                  role: 'cancel'
-                }
-              ]
-            });
-            alert.present();
-          });
-        });
-      this.loading = this.loadingCtrl.create();
-      this.loading.present();
-    }
+  updateProfile() {
+    //this.profileData.updateDOB(this.profileInfoForm.data.birthDate);
+    console.log(this.profileInfoForm);
+    this.profileData.updateAddress(this.profileInfoForm.data.address);
+    this.profileData.updateZip(this.profileInfoForm.data.zip);
+    this.profileData.updateCity(this.profileInfoForm.data.city);
+    this.profileData.updateCountry(this.profileInfoForm.data.country);
+    //this.navCtrl.setRoot(HandifyPage);
   }
+
 }
