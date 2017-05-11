555e76050d4505522a85093c15f3613bea57fd7e
mathiasKjeldsen <Mathias@mekbas.dk>
2017-05-11 22:59:35 +0200

    lul

--- src/pages/handify/handify.ts
+++ src/pages/handify/handify.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AuthData } from '../../providers/auth-data';
-import { HomePage } from '../home/home';
 import { ProfileData } from '../../providers/profile-data';
 import { CalendarPage } from '../calendar/calendar';
-
+import { LogOutPage } from '../log-out/log-out';
+import { ProfilePage } from '../profile/profile';
+import { ProfileSettingsPage } from '../profile-settings/profile-settings';
 @Component({
   selector: 'page-handify',
   templateUrl: 'handify.html'
@@ -12,21 +13,49 @@ import { CalendarPage } from '../calendar/calendar';
 export class HandifyPage {
   public userProfile: any;
   public birthDate: string;
+  pages: Array<{ title: string, component: any, icon: string, color: string, }>;
+
+
   constructor(public navCtrl: NavController, public authData: AuthData, public profileData: ProfileData) {
 
     this.profileData.getUserProfile().on('value', (data) => {
       this.userProfile = data.val();
       this.birthDate = this.userProfile.birthDate;
     });
+
+    this.pages = [
+      { title: 'Profile', component: ProfilePage, icon: "md-person", color: "#5AD864" },
+      { title: 'Calendar & tasks', component: CalendarPage, icon: "md-calendar", color: "#E57401" },
+      { title: 'Inbox', component: CalendarPage, icon: "md-mail", color: "#FCCE00" },
+      { title: 'Connect to helper', component: CalendarPage, icon: "md-contacts", color: "#AD5BEA" },
+      { title: 'Manage helpers', component: CalendarPage, icon: "md-paper", color: "#3FBECA" },
+      { title: 'Settings', component: ProfileSettingsPage, icon: "md-cog", color: "#D96A68" },
+      { title: 'Log out', component: LogOutPage, icon: "md-exit", color: "#BB8285" },
+
+
+      //{ title: 'Log out', component: HomePage, icon: "ios-exit-outline" }
+
+    ];
+
   }
 
-  logOut() {
-    this.authData.logoutUser().then(() => {
-      this.navCtrl.setRoot(HomePage);
-    });
+  openPage(page) {
+    this.navCtrl.setRoot(page.component);
+  }
+  goToLogOut() {
+    this.navCtrl.setRoot(LogOutPage);
   }
 
   goToCalendar() {
-    this.navCtrl.push(CalendarPage);
+    this.navCtrl.setRoot(CalendarPage);
   }
+
+  goToProfile() {
+    this.navCtrl.setRoot(ProfilePage);
+  }
+  goToSettings() {
+    this.navCtrl.push(ProfileSettingsPage);
+  }
+
+
 }
