b9231e43706c5077ba6d1a84850cf1d57648a5ae
bjornSkalkam <bjorn@skalkam.dk>
2017-05-13 20:46:14 +0200

    wtf is going on

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { InAppBrowser } from 'ionic-native';
 
 @Component({
   selector: 'page-tutorial',
