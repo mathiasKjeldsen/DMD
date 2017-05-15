f7a04b25605941d14b1d3a1c8356c4d01f74af98
bjornSkalkam <bjorn@skalkam.dk>
2017-05-15 17:32:06 +0200

    Made all numbers in may interactive

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -8,7 +8,7 @@ import { CalendarNewEventPage } from '../calendar-new-event/calendar-new-event';
 })
 export class CalendarPage {
 
-  public day = 20;
+  public day = 2;
   constructor(public navCtrl: NavController) {
 
   }
@@ -17,11 +17,97 @@ export class CalendarPage {
     this.navCtrl.push(CalendarNewEventPage);
   }
 
+  dayOne() {
+    this.day = 1;
+  }
+  dayTwo() {
+    this.day = 2;
+  }
+  dayThree() {
+    this.day = 3;
+  }
+  dayFour() {
+    this.day = 4;
+  }
+  dayFive() {
+    this.day = 5;
+  }
+  daySix() {
+    this.day = 6;
+  }
+  daySeven() {
+    this.day = 7;
+  }
+  dayEight() {
+    this.day = 8;
+  }
+  dayNine() {
+    this.day = 9;
+  }
+  dayTen() {
+    this.day = 10;
+  }
+  dayEleven() {
+    this.day = 11;
+  }
+  dayTwelve() {
+    this.day = 12;
+  }
+  dayThirteen() {
+    this.day = 13;
+  }
+  dayFourteen() {
+    this.day = 14;
+  }
+  dayFifteen() {
+    this.day = 15;
+  }
+  daySixteen() {
+    this.day = 16;
+  }
+  daySeventeen() {
+    this.day = 17;
+  }
+  dayEighteen() {
+    this.day = 18;
+  }
+  dayNineteen() {
+    this.day = 19;
+  }
   dayTwenty() {
     this.day = 20;
   }
-
   dayTwentyOne() {
     this.day = 21;
+  }
+  dayTwentyTwo() {
+    this.day = 22;
+  }
+  dayTwentyThree() {
+    this.day = 23;
+  }
+  dayTwentyFour() {
+    this.day = 24;
+  }
+  dayTwentyFive() {
+    this.day = 25;
+  }
+  dayTwentySix() {
+    this.day = 26;
+  }
+  dayTwentySeven() {
+    this.day = 27;
+  }
+  dayTwentyEight() {
+    this.day = 28;
+  }
+  dayTwentyNine() {
+    this.day = 29;
+  }
+  dayThirty() {
+    this.day = 30;
+  }
+  dayThirtyOne() {
+    this.day = 31;
   }
 }
