23817c768b31801a2c3e0e2104de53896907a751
bjornSkalkam <bjorn@skalkam.dk>
2017-05-15 16:27:18 +0200

    new calendar events now saver under firebase/handify-8783c/Calendar/this.currentUser.uid/Calenda Event xxxx where xxxx is a random id generated by the app

--- src/providers/calendar-data.ts
+++ src/providers/calendar-data.ts
@@ -6,17 +6,39 @@ export class CalendarData {
   public CalendarDatabase: firebase.database.Reference;
   public currentUser: firebase.User;
 
+  public EventNumber = 0;
+  public dateNumber = 0;
+  public randomLetter;
+  public randomLetterTwo;
+
   constructor() {
     this.currentUser = firebase.auth().currentUser;
-    this.CalendarDatabase = firebase.database().ref('/Calendar');
+    this.CalendarDatabase = firebase.database().ref('/Calendar/'+this.currentUser.uid);
   }
 
   newCalendarEvent(note: string, starts: string, ends: string, date: string): firebase.Promise<any> {
-    return this.CalendarDatabase.child(this.currentUser.uid).update({
+    //return this.CalendarDatabase.child(this.currentUser.uid).update({
+
+    this.randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    this.randomLetterTwo = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    this.dateNumber = Date.now();
+    this.EventNumber = this.dateNumber + this.randomLetter + this.randomLetterTwo;
+
+    //this.EventNumber = Date.now();
+    return this.CalendarDatabase.child("Calendar Event " + this.EventNumber).update({
       Date: date,
       Starts: starts,
       Ends: ends,
       Note: note,
     });
   }
+
+  //we could use this to generate unique id
+  guidGenerator() {
+    var S4 = function () {
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+  }
+
 }
