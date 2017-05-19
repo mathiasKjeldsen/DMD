63ede51d113a5f01b4bfb7d5f154bca4a2e7d819
bjornSkalkam <bjorn@skalkam.dk>
2017-05-19 22:32:39 +0200

    once again

--- src/pages/log-out/log-out.ts
+++ src/pages/log-out/log-out.ts
@@ -2,19 +2,16 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AuthData } from '../../providers/auth-data';
 import { HomePage } from '../home/home';
-import { ProfileData } from '../../providers/profile-data';
+
 @Component({
   selector: 'page-log-out',
   templateUrl: 'log-out.html'
 })
-export class LogOutPage {
-  public userProfile: any;
-  constructor(public navCtrl: NavController, public authData: AuthData, public profileData: ProfileData) {
 
-    this.profileData.getUserProfile().on('value', (data) => {
-      this.userProfile = data.val();
-    });
+export class LogOutPage {
+  constructor(public navCtrl: NavController, public authData: AuthData) {
   }
+
   logOut() {
     this.authData.logoutUser().then(() => {
       this.navCtrl.setRoot(HomePage);
